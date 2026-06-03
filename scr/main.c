de13e8cb882a032a9c64e623994c49c675619c31
Miguel <guelgomes2018@gmail.com>
2026-06-03 18:04:37 -0300

    Versão 0.1

---
+++ scr/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    int opcao;
+
+    do{
+        printf("\n===SISIPHUSBIBLIO===\n");
+        printf("\n1.Livros\n");
+        printf("\n2.Usuários");
+        printf("\n3.Empréstimos\n");
+        printf("\n4.Devoluções\n");
+        printf("\n5.Histórico\n");
+        printf("\n0.Sair\n");
+        scanf("%d", &opcao)
+        
+        switch(opcao){
+            case 1:
+            
+        }
+    }
+}
