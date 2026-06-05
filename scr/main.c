60dae9262376da3a1802a4c2b37b4040467af035
Miguel <guelgomes2018@gmail.com>
2026-06-05 14:15:14 -0300

    aaa

--- scr/main.c
+++ scr/main.c
@@ -3,49 +3,40 @@
 
 #define MAX 100
 
+struct Livro{
+    int codigo;
+    char titulo[200];
+    char autor[100];
+};
+
+struct Livro livros[MAX];
+int totalLivros = 0;
+
+void menuLivros();
+void cadastrarLivro();
+void listarLivros();
+
 int main(){
     setlocale(LC_ALL, "Portuguese");
-    int idsLivro[MAX];
-    char titulos[MAX][100];
-    char autores[MAX][100];
-    int ativo[MAX];
-    int totalLivros = 0;
-    int opcao, opcaoLivros;
-    int i, id, idx, encontrou, confirmacao;
-    char busca[100];
-
-    int idsUsuario[MAX];
-    char nomes[MAX][100];
-    char emails[MAX][100];
-    char telefones[MAX][20];
-    int ativoUsuario[MAX];
-    int totalUsuarios = 0;
-
+    
+    int opcao;
+    int opcaoLivros;
+   
     do{
         printf("\n===SISIPHUSBIBLIO===\n");
-        printf("\n1.Livros\n");
-        printf("\n2.Usuários");
-        printf("\n3.Empréstimos\n");
-        printf("\n4.Devoluções\n");
-        printf("\n5.Histórico\n");
-        printf("\n0.Sair\n");
+        printf("\n1. Livros");
+        printf("\n2. Usuários");
+        printf("\n3. Empréstimos");
+        printf("\n4. Devoluções");
+        printf("\n5. Histórico");
+        printf("\n0. Sair\n");
+        printf("\nEscolha uma opção: ");
         scanf("%d", &opcao);
         
         switch(opcao){
         case 1:
-            printf("\n1.Cadastrar livros\n");
-            printf("\n2.Listar livros\n");
-            printf("\n3.Buscar livro\n");
-            printf("\n4.Editar livro\n");
-            printf("\n5.Ativar/Desativar livro\n");
-            printf("\n0.Voltar\n");
-            scanf("%d", &opcaoLivros);
-
-            switch(opcaoLivros){
-                case 1:
-                printf("\n")
-            }
-            break;
+           menuLivros();
+           break;
 
         case 2:
             printf("\n1.Cadastrar usuário\n");
@@ -54,17 +45,23 @@ int main(){
             printf("\n4.Editar informação de usuário\n");
             printf("\n5.Ativar/Desativar usuário\n");
             printf("\n0.Voltar\n");
+            printf("\nEscolha uma opção: ");
+            scanf("%d", &opcaoLivros);
             break;
 
         case 3:
             printf("\n1.Realizar empréstimo\n");
             printf("\n2.Listar empréstimos ativos\n");
             printf("\n0.Voltar\n");
+            printf("\nEscolha uma opção: ");
+            scanf("%d", &opcaoLivros);
             break;
 
         case 4:
             printf("\n1.Registrar Devolução\n");
             printf("\n0.Voltar");
+            printf("\nEscolha uma opção: ");
+            scanf("%d", &opcaoLivros);
             break;
 
         case 5:
@@ -72,8 +69,50 @@ int main(){
             printf("\n2.Filtrar por usuário\n");
             printf("\n3.Filtrar por livro\n");
             printf("\n0.Voltar\n");
+            printf("\nEscolha uma opção: ");
+            scanf("%d", &opcaoLivros);
             break;
-        
+        default:
+        printf("\nOpção inválida...\n");
         }
+    } while (opcao != 0);
+}
+
+void menuLivros(){
+
+    int opcaoLivros;
+ 
+    do{
+            printf("\n1. Cadastrar livros");
+            printf("\n2. Listar livros");
+            printf("\n3. Buscar livro");
+            printf("\n4. Editar livro");
+            printf("\n5. Ativar/Desativar livro");
+            printf("\n0. Voltar");
+            printf("\nEscolha uma opção: ");
+            scanf("%d", &opcaoLivros);
+            break;
     }
+}while (opcao != 0);
+
+void cadastrarLivro(){
+
+    char titulo[100];
+    char autor[100];
+
+    getchar();
+
+    printf("\n===CADASTRO DE LIVROS===\n");
+
+    printf("Titulo: ")
+    fgets("titulo,sizeof(titulo), stdin");
+
+    printf("Autor: ")
+    fgets("autor,sizeof(autor), stdin");
+
+    printf("\n=== DADOS INFORMADOS ===\n");
+    printf("Titulo: %s", titulo);
+    printf("Autor: %s", autor);
+
+    printf("\nLivro cadastrado com sucesso!\n");
 }
